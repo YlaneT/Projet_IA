c5ac83d489d45e9dcb34a837f5873c3d055e5184
YlaneT <yloo318@yahoo.fr>
2020-04-04 19:26:30 +0200

    Ajout d'un jeu de Othello fonctionnel dans le module Tags

--- Trunk/src/MainClass.java
+++ Trunk/src/MainClass.java
@@ -4,7 +4,7 @@ import Interfaces.Interface_Othello;
 public class MainClass {
 
 	public static void main (String[]args) {
-		Interface_Jeu jeu = new Jeu (new Othello());
+		Jeu jeu = new Jeu (new Othello());
 		while (!jeu.plateau.grillePleine()) {
 			char symbole = jeu.prochainJoueur();
 			jeu.tourJoueur(symbole);
